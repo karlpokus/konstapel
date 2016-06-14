b4bacd44106f937553b7d43a051bf00468f61fe3
Karl Pokus <karl.pokus@gmail.com>
2016-06-14 07:32:02 +0200

    working draft

---
+++ konstapel.js
@@ -0,0 +1,128 @@
+var Cryptr = require('cryptr'),
+    Knas = require('knas');
+
+var K = module.exports = function(tokenKey, signupKey) {
+  if (!tokenKey) {
+    throw new Error("tokenKey required");
+  }
+  if (signupKey) {
+    this.opts = {
+      signupKey: signupKey
+    };
+  }
+  this.cryptr = new Cryptr(tokenKey);
+}
+
+// LOGIN flow
+
+K.prototype.findUserByUsername = function(req, res, next) {
+  this.users.findOne({username: req.body.user}, function(err, doc) { // doc is {} or null
+    if (err) return next(err);
+
+    req.user = doc;
+    return next();
+  });
+}
+
+K.prototype.usernameIsValid = function(req, res, next) {
+  if (!req.user) {
+    return next(new Knas(401, 'Invalid username'));
+  }
+  return next();
+}
+
+K.prototype.pwdIsValid = function(req, res, next) {
+  this.bcrypt.compare(req.body.pwd, req.user.pwd, function(err, res) {
+    if (err) return next(err);
+
+    if (!res) {
+      return next(new Knas(401, 'Invalid password'));
+    }
+    return next();
+  });
+}
+
+// SIGNUP flow
+
+K.prototype.checkSignupKey = function(req, res, next) {
+  if (req.body.key !== this.opts.signupKey) {
+    return next(new Knas(401, 'Invalid key'));
+  }
+  return next();
+}
+
+K.prototype.findUsers = function(req, res, next) {
+  this.users.find({}, {username: 1, _id: 0})
+    .map(function(doc){
+      return doc.username;
+    })
+    .toArray(function(err, docs){
+      if (err) return next(err);
+
+      req.temp = {usernames: docs};
+      return next();
+    });
+}
+
+K.prototype.usernameNotTaken = function(req, res, next) {
+  if (req.temp.usernames.indexOf(req.body.user) < 0) {
+    return next();
+  } else {
+    return next(new Knas(400, 'Username taken'));
+  }
+}
+
+K.prototype.insertUser = function(req, res, next) {
+  var self = this;
+
+  this.bcrypt.hash(req.body.pwd, 10, function(err, hash) {
+    if (err) return next(err);
+
+    var doc = {
+      username: req.body.user,
+      pwd: hash
+    };
+
+    self.users.insert(doc, function(err, docs){
+      if (err) return next(err);
+
+      req.user = docs.ops[0];
+      return next();
+    });
+  });
+}
+
+K.prototype.createToken = function(req, res, next) {
+  var token = this.cryptr.encrypt(req.user._id);
+  req.data = {token: token, username: req.user.username};
+  return next();
+}
+
+K.prototype.verifyToken = function(req, res, next) {
+  var token = req.body.token || req.query.token || false;
+  if (!token) {
+    return next(new Knas(401, 'Not authorized'));
+  }
+
+  try {
+    var userId = this.cryptr.decrypt(token);
+    req.temp = {id: userId};
+    return next();
+  } catch (err) {
+    return next(new Knas(401, 'Invalid token'));
+  }
+}
+
+K.prototype.findUserById = function(req, res, next) {
+  var oid = this.oid(req.temp.id);
+
+  this.users.findOne({_id: oid}, function(err, doc) { // doc is {} or null
+    if (err) return next(err);
+
+    if (!doc) {
+      return next(new Knas(401, 'Invalid token'));
+    }
+    req.user = doc;
+    return next();
+  });
+}
---
+++ test/tests.js
@@ -0,0 +1,92 @@
+var test = require('tape'),
+    Konstapel = require('../konstapel.js'),
+    klang = new Konstapel('tokenKey', 'signupKey');
+
+test('constructor', function(t){
+  function noArgs() {
+    var kling = new Konstapel();
+  }
+  t.throws(noArgs, /tokenKey required/, 'throws on no tokenKey')
+  t.equal(klang.opts.signupKey, 'signupKey', '.opts.signupKey');
+  t.equal(typeof klang.cryptr.encrypt, 'function', '.cryptr.encrypt');
+  t.equal(typeof klang.cryptr.decrypt, 'function', '.cryptr.decrypt');
+  t.end();
+});
+
+test('.usernameIsValid', function(t){
+  var pass = klang.usernameIsValid({user: {}}, null, function(){
+        return true;
+      }),
+      fail = klang.usernameIsValid({}, null, function(err){
+        return err;
+      });
+
+  t.equal(pass, true, 'calls next if req.user');
+  t.equal(fail instanceof Error, true, 'returns error if !req.user');
+  t.end();
+});
+
+test('.checkSignupKey', function(t){
+  var pass = klang.checkSignupKey({body: {key: 'signupKey'}}, null, function(){
+        return true;
+      }),
+      fail = klang.checkSignupKey({body: {key: ''}}, null, function(err){
+        return err;
+      });
+
+  t.equal(pass, true, 'calls next on a match');
+  t.equal(fail instanceof Error, true, 'returns error on no match');
+  t.end();
+});
+
+test('.usernameNotTaken', function(t){
+  var reqPass = {
+        temp: {usernames: ['lucy', 'john', 'erin']},
+        body: {user: 'mike'}
+      },
+      reqFail = {
+        temp: {usernames: ['lucy', 'john', 'mike']},
+        body: {user: 'mike'}
+      },
+      pass = klang.usernameNotTaken(reqPass, null, function(){
+        return true;
+      }),
+      fail = klang.usernameNotTaken(reqFail, null, function(err){
+        return err;
+      });
+
+  t.equal(pass, true, 'calls next if no match');
+  t.equal(fail instanceof Error, true, 'returns error on a match');
+  t.end();
+});
+
+test('.createToken', function(t){
+  var req = {
+        user: {_id: 'abc123', username: 'mike'}
+      },
+      pass = klang.createToken(req, null, function(){
+        return true;
+      });
+
+  t.equal(req.data.token, '806a7f191e18', 'token');
+  t.equal(req.data.username, 'mike', 'username');
+  t.equal(pass, true, 'calls next');
+  t.end();
+});
+
+test('.verifyToken', function(t){
+  var req = {
+        body: {token: '806a7f191e18'}
+      },
+      pass = klang.verifyToken(req, null, function(){
+        return true;
+      });
+
+  t.equal(req.temp.id, 'abc123', 'decrypts userId');
+  t.equal(pass, true, 'calls next');
+
+  // test for no token
+  // test for bad token
+  
+  t.end();
+});
